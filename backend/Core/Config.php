cdbd78e8b719062a79003bed23b450068291d87d
samuelsilvadonascimento9-svg <samuelsilvadonascimento9@gmail.com>
2026-05-05 20:47:58 -0300

    Add chatbot IA sem a chave de seguranca

--- backend/Core/Config.php
+++ backend/Core/Config.php
@@ -12,7 +12,7 @@ const DB_CONFIG = [
 
 // backend/Core/Config.php
 const OPENROUTER_CONFIG = [
-    'api_key' => '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',
+    'api_key' => 'API aqui',
     'model'   => 'openai/gpt-oss-120b:free', 
 ];
 
